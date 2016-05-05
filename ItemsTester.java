b5713479de268060a03c77622d8c015b25fed041
izzyimac <isabelleaimacseng@gmail.com>
2016-05-05 10:53:00 -0700

    Added files via upload

---
+++ ItemsTester.java
@@ -0,0 +1,21 @@
+package project;
+
+public class ItemsTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		KitchenWare fork = new KitchenWare();
+		System.out.println(fork);
+		fork.setBrand("KitchenBasics");
+		fork.setPrice(2.00);
+		fork.setType("fork");
+		fork.setRating("one");
+		System.out.println(fork);
+		
+		
+		
+
+	}
+
+}
