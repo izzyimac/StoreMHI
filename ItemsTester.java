8b0c77b66109563737648010929aa51380c4b0f0
hawalker <hannahwalker1111@gmail.com>
2016-05-05 11:10:25 -0700

    Update ItemsTester.java

--- ItemsTester.java
+++ ItemsTester.java
@@ -3,8 +3,7 @@ package project;
 public class ItemsTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+
 		System.out.println("TESTING KITCHENWARE");
 		KitchenWare fork = new KitchenWare();
 		System.out.println(fork);
