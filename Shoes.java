0391c2ba5d8d41e42e905c7341561df0f1b4abbf
michelledzhao <michelledzhao@gmail.com>
2016-05-05 10:43:55 -0700

    Update Shoes.java

--- Shoes.java
+++ Shoes.java
@@ -1,13 +1,66 @@
+package DepartmentStore;
 
 import java.awt.Color;
 
-public class Shoes {
+public class Shoes extends Item implements Basic{
 	public String name;
 	public double price;
 	public String brand;
 	public String type;
 	public double size;
+	
+public Shoes(){
+		
+	}
+	public Shoes(String name, double price, String brand, String type, double size){
+		setName(name);
+		
+	}
+	
+	public double getSize(){
+		return size;
+	}
+	
+	public String getName() {
+		return name;
+	}
 
+	public String getBrand() {
+		return brand;
+	}
+
+	public String getType() {
+		return type;
+	}
+	
+	public double getPrice(){
+		return price;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public void setSize(double size) {
+		this.size = size;
+	}
+
+	public String toString(){
+		return "Name: " + getName() + "Brand: " + getBrand() + "Type: "+ getType()+ "Price: "+ getPrice() + "Size: "+ getSize();
+	}
+	
 		
 	
 }
